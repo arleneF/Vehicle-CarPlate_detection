f4950de796507801aa03badd3d8afd2a3a3e05cb
Arlene Fu <arlenelalala@gmail.com>
2017-11-17 20:49:22 -0800

    define layers using keras through tensorflow

--- vehicle-detection/visualizations.py
+++ vehicle-detection/visualizations.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-import cv2
+
 
 
 def draw_speed(img_cp, fps, w):
@@ -11,15 +11,15 @@ def draw_speed(img_cp, fps, w):
     cv2.line(img_cp,(w-160,0),(w-160,155),(255,0,0),5)
 
 
-def draw_thumbnails(img_cp, img, window_list, thumb_w=100, thumb_h=80, off_x=30, off_y=30):
+def draw_thumbnails(img_cp, img, window_list ,counter):
 
-    cv2.putText(img_cp, 'Detected viehicles', (400,37), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255,255,0), 2, cv2.LINE_AA)
+    #cv2.putText(img_cp, 'Detected viehicles', (400,37), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255,255,0), 2, cv2.LINE_AA)
     for i, bbox in enumerate(window_list):
         thumbnail = img[bbox[0][1]:bbox[1][1], bbox[0][0]:bbox[1][0]]
-        cv2.imwrite('croppedImg/sample.jpg',thumbnail)
-        vehicle_thumb = cv2.resize(thumbnail, dsize=(thumb_w, thumb_h))
-        start_x = 300 + (i+1) * off_x + i * thumb_w
-        img_cp[off_y + 30:off_y + thumb_h + 30, start_x:start_x + thumb_w, :] = vehicle_thumb
+        cv2.imwrite('croppedImg/sample%d_%d.jpg'%(counter,i),cv2.cvtColor(thumbnail, cv2.COLOR_RGB2BGR))
+        #vehicle_thumb = cv2.resize(thumbnail, dsize=(thumb_w, thumb_h))
+        #start_x = 300 + (i+1) * off_x + i * thumb_w
+        #img_cp[off_y + 30:off_y + thumb_h + 30, start_x:start_x + thumb_w, :] = vehicle_thumb
 
 
 def draw_background_highlight(image, w):
